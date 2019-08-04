8257b7b178073f05858fe51fca1dc1a2698cbca5
juromi <mihaela.ascic@gmail.com>
2019-08-04 13:47:30 +0200

    2. Register form started

--- register/index.php
+++ register/index.php
@@ -2,3 +2,35 @@
 
 // Included header
 require '../header/header_out.php';
+
+// define variables and set to empty values
+$name = $email = $password = $confirm_password "";
+
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $password = test_input($_POST["password"]);
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+}
+?>
+
+<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+  Name: <input type="text" name="name">
+  <br><br>
+  E-mail: <input type="text" name="email">
+  <br><br>
+  Password: <input type="text" name="password">
+
+  <br><br>
+  <input type="submit" name="submit" value="Submit">
+</form>
+
+
+
+</body>
+</html>
